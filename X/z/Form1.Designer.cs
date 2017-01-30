2d2eefada1c54a1aff260aa1973b6a214f64bb7b
Катя <@->
2017-01-30 15:10:03 +0700

    1111

---
+++ X/z/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace z
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.курсПотокBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.факультетКурсBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.факультетBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.database2DataSet = new z.Database2DataSet();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.факультетTableAdapter = new z.Database2DataSetTableAdapters.ФакультетTableAdapter();
+            this.курсTableAdapter = new z.Database2DataSetTableAdapters.КурсTableAdapter();
+            this.потокTableAdapter = new z.Database2DataSetTableAdapters.ПотокTableAdapter();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Время = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.курсПотокBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.факультетКурсBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.факультетBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database2DataSet)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.comboBox3);
+            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(689, 85);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(523, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 77);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Загрузить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DataSource = this.курсПотокBindingSource;
+            this.comboBox3.DisplayMember = "Имя потока";
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(318, 29);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 2;
+            // 
+            // курсПотокBindingSource
+            // 
+            this.курсПотокBindingSource.DataMember = "КурсПоток";
+            this.курсПотокBindingSource.DataSource = this.факультетКурсBindingSource;
+            // 
+            // факультетКурсBindingSource
+            // 
+            this.факультетКурсBindingSource.DataMember = "ФакультетКурс";
+            this.факультетКурсBindingSource.DataSource = this.факультетBindingSource;
+            // 
+            // факультетBindingSource
+            // 
+            this.факультетBindingSource.DataMember = "Факультет";
+            this.факультетBindingSource.DataSource = this.database2DataSet;
+            // 
+            // database2DataSet
+            // 
+            this.database2DataSet.DataSetName = "Database2DataSet";
+            this.database2DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DataSource = this.факультетКурсBindingSource;
+            this.comboBox2.DisplayMember = "Номер курса";
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(170, 29);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataSource = this.факультетBindingSource;
+            this.comboBox1.DisplayMember = "Имя";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(22, 30);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 103);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(689, 250);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Расписание";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Время});
+            this.dataGridView1.Location = new System.Drawing.Point(6, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(677, 225);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            // 
+            // факультетTableAdapter
+            // 
+            this.факультетTableAdapter.ClearBeforeFill = true;
+            // 
+            // курсTableAdapter
+            // 
+            this.курсTableAdapter.ClearBeforeFill = true;
+            // 
+            // потокTableAdapter
+            // 
+            this.потокTableAdapter.ClearBeforeFill = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(417, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Время
+            // 
+            this.Время.Frozen = true;
+            this.Время.HeaderText = "Время";
+            this.Время.Name = "Время";
+            this.Время.ReadOnly = true;
+            this.Время.Width = 50;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 365);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Расписание";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.курсПотокBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.факультетКурсBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.факультетBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database2DataSet)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private Database2DataSet database2DataSet;
+        private System.Windows.Forms.BindingSource факультетBindingSource;
+        private Database2DataSetTableAdapters.ФакультетTableAdapter факультетTableAdapter;
+        private System.Windows.Forms.BindingSource факультетКурсBindingSource;
+        private Database2DataSetTableAdapters.КурсTableAdapter курсTableAdapter;
+        private System.Windows.Forms.BindingSource курсПотокBindingSource;
+        private Database2DataSetTableAdapters.ПотокTableAdapter потокTableAdapter;
+        public System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Время;
+    }
+}
+
